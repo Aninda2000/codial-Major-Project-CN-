948900e0261a1f52300215b35d6ec32adc504c6f
Aninda2000 <anindaroy100@gmail.com>
2022-07-13 00:19:31 +0530

    add user controller and routes and set up view engine

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port=8000;
 //use the router middleware
 app.use(require('./routes/index'));//by default it always goes to index
 
+//set up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error : ${err}`);
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router=express.Router();
 const homeController=require('../controllers/home_controller');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
 
 module.exports=router;
