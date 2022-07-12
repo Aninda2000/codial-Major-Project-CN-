af6748a543454ba3b4d74396d93509043dfb39bf
Aninda2000 <anindaroy100@gmail.com>
2022-07-12 23:39:45 +0530

    adding home controller and routes

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home=function(req,res){
+    res.end("<h1>Express is up and running</h1>");
+}
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express= require('express');
 const app=express();
 const port=8000;
 
+//use the router middleware
+app.use(require('./routes/index'));//by default it always goes to index
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error : ${err}`);
